0aa43677eaae939a1972cca3598a40090ab13afd
Mila <milaarroyo65@gmail.com>
2025-09-26 19:01:48 -0600

    remove unused import statements and comments

--- routes/books.js
+++ routes/books.js
@@ -1,17 +1,13 @@
 import express from "express";
-import axios from "axios";
-import db from "../db/db.js";
-import { marked } from "marked"; //converts markdown to html
 import { getBook } from "../lib/api.js";
 import { getBookById } from "../lib/queries.js";
 
 const router = express.Router();
 const base_url = "https://openlibrary.org"; /* add .json at the end */
 
-// ---- TEST ----
+
 router.get("/book/:id", async (req, res) => {
 	const bookId = req.params.id;
-	// console.log("Book ID: ", bookId);
 
 	try {
 		if (bookId.includes("OL")) {
@@ -24,7 +20,7 @@ router.get("/book/:id", async (req, res) => {
 				month: "long",
 				year: "numeric",
 			});
-			// console.log(book.created_at);
+
 			res.render("details", { book, isFromApi: false });
 		}
 	} catch (error) {
@@ -42,7 +38,7 @@ router.get("/book/:id/add", async (req, res) => {
 
 	try {
 		const book = await getBook("/work/" + bookId);
-		// console.log(book);
+
 		res.render("add-book", { book });
 	} catch (error) {
 		console.error(error);
@@ -57,7 +53,6 @@ router.get("/book/:id/add", async (req, res) => {
 router.get("/book/:id/edit", async (req, res) => {
 	try {
 		const book = await getBookById(req.params.id);
-		// console.log(book);
 
 		res.render("edit-book", { book });
 	} catch (error) {
